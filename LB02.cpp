c3e0c73c752ec2fd34aa71b232b34684add597e3
floweryada <floweryada@gmail.com>
2014-04-09 09:53:32 +0300

    f

--- LB02.cpp
+++ LB02.cpp
@@ -7,10 +7,10 @@
 
 using namespace std;
 
-void enterVectors (struct Vector a, struct Vector b)
+void enterVector (Vector a)
 
 {
-    cout << "Enter Coordinates of two Vectors: \n";
+    cout << "Enter Coordinate of Vector: \n";
     cin.ignore(1,'\n');
 
         cout << "X Coordinate Of Vector: ";
@@ -23,68 +23,52 @@ void enterVectors (struct Vector a, struct Vector b)
         cin >> a.coordinateZ;
         cout << endl;
 
-        cout << "X Coordinate Of Vector: ";
-        cin >> b.coordinateX;
-
-        cout << "Y Coordinate Of Vector: ";
-        cin >> b.coordinateY;
-
-        cout << "Z Coordinate Of Vector: ";
-        cin >> b.coordinateZ;
-        cout << endl;
 }
 
-void sumVectors ( Vector a, Vector b, Vector c)
+Vector sumVectors (Vector a, Vector b)
 {
+            Vector c;
             c.coordinateX = a.coordinateX + b.coordinateX,
             c.coordinateY = a.coordinateY + b.coordinateX,
             c.coordinateZ = a.coordinateZ + b.coordinateX;
-            outVectors(c);
+            return c;
 }
 
-void outVectors(Vector c)
+Vector outVector(Vector c)
 {
     cout << "Coordinates of new Vector: \n";
     cout << c.coordinateX << " " << c.coordinateY << " " << c.coordinateX << endl;
 }
 
-void subctractVectors (Vector a, Vector b, Vector c)
+Vector subctractVectors (Vector a, Vector b)
 {
-            c.coordinateX = a.coordinateX;
-            c.coordinateY = a.coordinateY;
-            c.coordinateZ = a.coordinateZ;
+            Vector c;
             c.coordinateX = a.coordinateX - b.coordinateX;
             c.coordinateY = a.coordinateY - b.coordinateX;
             c.coordinateZ = a.coordinateZ - b.coordinateX;
-            outVectors(c);
+            return c;
 }
 
-void multiplyVectorByScalar (Vector a, Vector b, int scalar, Vector c, Vector d)
+Vector multiplyVectorByScalar (Vector a, int scalar)
 {
+    Vector c;
     c.coordinateX = scalar * a.coordinateX,
     c.coordinateY = scalar * a.coordinateY,
-    c.coordinateZ = scalar * a.coordinateZ,
-    d.coordinateX = scalar * b.coordinateX,
-    d.coordinateY = scalar * b.coordinateY,
-    d.coordinateZ = scalar * b.coordinateZ;
-
-    cout << "Coordinates of new Vectors: \n" << "The First: ";
-    outVectors(c);
-    cout << endl;
-    cout << "The Second: ";
-    outVectors(d);
+    c.coordinateZ = scalar * a.coordinateZ;
+    return c;
 }
 
-void scalarProductOfVectors (Vector a, Vector b)
+Vector scalarProductOfVectors (Vector a, Vector b)
 {
     cout << "scalar product of two Vectors: ";
     cout << a.coordinateX * b.coordinateX + a.coordinateY * b.coordinateY + a.coordinateZ * b.coordinateZ;
 }
 
-void vectorProductOfVectors (Vector a, Vector b, Vector c)
+Vector vectorProductOfVectors (Vector a, Vector b)
 {
+    Vector c;
     c.coordinateX = a.coordinateY * b.coordinateZ - b.coordinateY * a.coordinateZ;
     c.coordinateY = a.coordinateX * b.coordinateZ - b.coordinateX * a.coordinateZ;
     c.coordinateZ = a.coordinateX * b.coordinateY - b.coordinateX * a.coordinateY;
-    outVectors(c);
+    return c;
 }
